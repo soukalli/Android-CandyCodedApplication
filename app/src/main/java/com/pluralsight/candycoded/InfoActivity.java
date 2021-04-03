cfb02c9338390d349c099e3484f96847a5309039
Abdel Kader FALL <abdoulkader.fall>
2021-04-03 17:52:15 +0000

    start the google maps activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,10 +1,14 @@
 package com.pluralsight.candycoded;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
 import android.widget.ImageView;
 import com.squareup.picasso.Picasso;
 import java.io.IOException;
@@ -31,6 +35,17 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+
+    public void createMapIntent(View view){
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        ComponentName activity = mapIntent.resolveActivity(getPackageManager());
+        if (activity != null){
+            startActivity(mapIntent);
+        }
+
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
